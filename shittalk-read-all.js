35e18657ab40aab43a539ce16ed7c561884fca87
Davis Ford <ford_davis@bah.com>
2019-03-08 18:28:12 -0500

    Added /all

--- shittalk-read-all.js
+++ shittalk-read-all.js
@@ -1,20 +1,37 @@
 'use strict';
 
+const _ = require('lodash');
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+/**
+ * Consolidates the front end requests
+ * {
+ *  random: [],
+ *  recent: [],
+ *  top: [],
+ * }
+ */
 module.exports = (event, callback) => {
   const params = {
     TableName: 'shittalk',
+    ConsistentRead: false,
   };
 
-  return dynamoDb.scan(params, (error, data) => {
+  return dynamoDb.scan(params, (error, pData) => {
     if (error) {
       return callback(error, { error, data: [] });
     }
-    if (!data || !data.Items) {
+    if (!pData || !pData.Items) {
       callback(error, { error, data: [] });
     }
-    return callback(error, { error, data: data.Items });
+
+    const NUM_ITEMS = 20;
+    const data = {
+      random: _.sampleSize(pData.Items, NUM_ITEMS),
+      recent: pData.Items.sort((a, b) => b.createdAt - a.createdAt).slice(0, NUM_ITEMS),
+      top: pData.Items.sort((a, b) => b.net_votes - a.net_votes).slice(0, NUM_ITEMS),
+    }
+    return callback(error, { error, data });
   });
 };
